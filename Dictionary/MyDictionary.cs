e45527e9a7634eb1cd6dc7be1b822214322edc73
edim28 <ylmazadem898@gmail.com>
2021-07-02 19:41:42 +0300

    Add project files.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class MyDictionary<K,V>
+    {
+        K[] keys;
+        K[] tempKeys;
+        V[] values;
+        V[] tempValues;
+        public MyDictionary()
+        {
+            keys = new K[0];
+            values = new V[0];
+
+        }
+        public void Add(K key,V value)
+        {
+            tempKeys = keys;
+            tempValues = values;
+
+            keys = new K[keys.Length + 1];
+            values = new V[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+            }
+
+            keys[keys.Length - 1] = key;
+
+            for (int i = 0; i < tempValues.Length; i++)
+            {
+                values[i] = tempValues[i];
+            }
+
+            values[values.Length - 1] = value;
+        }
+        public int Length
+        {
+            get { return keys.Length; }
+        }
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> ögrenciler = new MyDictionary<int, string>();
+            ögrenciler.Add(61, "Adem");
+            ögrenciler.Add(28, "Arif");
+            ögrenciler.Add(34, "Bora");
+            ögrenciler.Add(54, "Şakir");
+
+            Console.WriteLine(ögrenciler.Length);
+            Console.ReadKey();
+        }
+    }
+}
